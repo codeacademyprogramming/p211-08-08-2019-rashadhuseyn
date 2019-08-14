04c2190a2a5700d40fb5397f4c32eecc35c244f0
rashadhuseyn <49521003+rashadhuseyn@users.noreply.github.com>
2019-08-14 12:16:24 +0400

    Add files via upload

---
+++ gametime/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace gametime
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.snake = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DarkKhaki;
+            this.button1.Location = new System.Drawing.Point(69, 420);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(125, 107);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Left";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Coral;
+            this.button2.Location = new System.Drawing.Point(247, 420);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(125, 107);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "RIGHT";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.button3.Location = new System.Drawing.Point(160, 307);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(125, 107);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "UP";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Cyan;
+            this.button4.Location = new System.Drawing.Point(160, 533);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(125, 107);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "DOWN";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.Button4_Click);
+            // 
+            // snake
+            // 
+            this.snake.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.snake.Location = new System.Drawing.Point(653, 256);
+            this.snake.Name = "snake";
+            this.snake.Size = new System.Drawing.Size(141, 43);
+            this.snake.TabIndex = 4;
+            this.snake.Text = "I am snake :)";
+            this.snake.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1323, 652);
+            this.Controls.Add(this.snake);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button snake;
+    }
+}
+
---
+++ gametime/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace gametime
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            int n1 = snake.Location.X;
+            int n2 = snake.Location.Y - 10;
+            snake.Location = new Point(n1, n2);
+        }
+
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            int n1 = snake.Location.X;
+            int n2 = snake.Location.Y + 10;
+            snake.Location = new Point(n1, n2);
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            int n1 = snake.Location.X + 10;
+            int n2 = snake.Location.Y;
+            snake.Location = new Point(n1, n2);
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            int n1 = snake.Location.X - 10;
+            int n2 = snake.Location.Y;
+            snake.Location = new Point(n1, n2);
+        }
+    }
+}
