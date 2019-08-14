9ed10af259166b377cd1e19428db6d842b4990b9
rashadhuseyn <49521003+rashadhuseyn@users.noreply.github.com>
2019-08-14 13:37:54 +0400

    Add files via upload

---
+++ sinema/Form1.Designer.cs
@@ -0,0 +1,371 @@
+﻿namespace sinema
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.button14 = new System.Windows.Forms.Button();
+            this.button15 = new System.Windows.Forms.Button();
+            this.button16 = new System.Windows.Forms.Button();
+            this.button17 = new System.Windows.Forms.Button();
+            this.button18 = new System.Windows.Forms.Button();
+            this.button19 = new System.Windows.Forms.Button();
+            this.count = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Price = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // Button1
+            // 
+            this.Button1.BackColor = System.Drawing.Color.Gold;
+            this.Button1.Location = new System.Drawing.Point(268, 12);
+            this.Button1.Name = "Button1";
+            this.Button1.Size = new System.Drawing.Size(99, 75);
+            this.Button1.TabIndex = 0;
+            this.Button1.Text = "Select your seat please";
+            this.Button1.UseVisualStyleBackColor = false;
+            this.Button1.Click += new System.EventHandler(this.Button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Gold;
+            this.button2.Location = new System.Drawing.Point(326, 93);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(99, 75);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Select your seat please";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Gold;
+            this.button3.Location = new System.Drawing.Point(629, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(99, 75);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Select your seat please";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Gold;
+            this.button4.Location = new System.Drawing.Point(506, 12);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(99, 75);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Select your seat please";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.Button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.Gold;
+            this.button5.Location = new System.Drawing.Point(383, 12);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(99, 75);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "Select your seat please";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.Button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.Gold;
+            this.button6.Location = new System.Drawing.Point(867, 12);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(99, 75);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "Select your seat please";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.Button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.Gold;
+            this.button7.Location = new System.Drawing.Point(823, 93);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(99, 75);
+            this.button7.TabIndex = 7;
+            this.button7.Text = "Select your seat please";
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.Button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.Color.Gold;
+            this.button8.Location = new System.Drawing.Point(700, 93);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(99, 75);
+            this.button8.TabIndex = 8;
+            this.button8.Text = "Select your seat please";
+            this.button8.UseVisualStyleBackColor = false;
+            this.button8.Click += new System.EventHandler(this.Button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.BackColor = System.Drawing.Color.Gold;
+            this.button9.Location = new System.Drawing.Point(578, 93);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(99, 75);
+            this.button9.TabIndex = 9;
+            this.button9.Text = "Select your seat please";
+            this.button9.UseVisualStyleBackColor = false;
+            this.button9.Click += new System.EventHandler(this.Button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.BackColor = System.Drawing.Color.Gold;
+            this.button10.Location = new System.Drawing.Point(454, 93);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(99, 75);
+            this.button10.TabIndex = 10;
+            this.button10.Text = "Select your seat please";
+            this.button10.UseVisualStyleBackColor = false;
+            this.button10.Click += new System.EventHandler(this.Button10_Click);
+            // 
+            // button11
+            // 
+            this.button11.BackColor = System.Drawing.Color.Gold;
+            this.button11.Location = new System.Drawing.Point(652, 174);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(99, 75);
+            this.button11.TabIndex = 11;
+            this.button11.Text = "Select your seat please";
+            this.button11.UseVisualStyleBackColor = false;
+            this.button11.Click += new System.EventHandler(this.Button11_Click);
+            // 
+            // button12
+            // 
+            this.button12.BackColor = System.Drawing.Color.Gold;
+            this.button12.Location = new System.Drawing.Point(527, 174);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(99, 75);
+            this.button12.TabIndex = 12;
+            this.button12.Text = "Select your seat please";
+            this.button12.UseVisualStyleBackColor = false;
+            this.button12.Click += new System.EventHandler(this.Button12_Click);
+            // 
+            // button13
+            // 
+            this.button13.BackColor = System.Drawing.Color.Gold;
+            this.button13.Location = new System.Drawing.Point(396, 174);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(99, 75);
+            this.button13.TabIndex = 13;
+            this.button13.Text = "Select your seat please";
+            this.button13.UseVisualStyleBackColor = false;
+            this.button13.Click += new System.EventHandler(this.Button13_Click);
+            // 
+            // button14
+            // 
+            this.button14.BackColor = System.Drawing.Color.Gold;
+            this.button14.Location = new System.Drawing.Point(454, 255);
+            this.button14.Name = "button14";
+            this.button14.Size = new System.Drawing.Size(99, 75);
+            this.button14.TabIndex = 14;
+            this.button14.Text = "Select your seat please";
+            this.button14.UseVisualStyleBackColor = false;
+            this.button14.Click += new System.EventHandler(this.Button14_Click);
+            // 
+            // button15
+            // 
+            this.button15.BackColor = System.Drawing.Color.Gold;
+            this.button15.Location = new System.Drawing.Point(700, 255);
+            this.button15.Name = "button15";
+            this.button15.Size = new System.Drawing.Size(99, 75);
+            this.button15.TabIndex = 15;
+            this.button15.Text = "Select your seat please";
+            this.button15.UseVisualStyleBackColor = false;
+            this.button15.Click += new System.EventHandler(this.Button15_Click);
+            // 
+            // button16
+            // 
+            this.button16.BackColor = System.Drawing.Color.Gold;
+            this.button16.Location = new System.Drawing.Point(769, 174);
+            this.button16.Name = "button16";
+            this.button16.Size = new System.Drawing.Size(99, 75);
+            this.button16.TabIndex = 16;
+            this.button16.Text = "Select your seat please";
+            this.button16.UseVisualStyleBackColor = false;
+            this.button16.Click += new System.EventHandler(this.Button16_Click);
+            // 
+            // button17
+            // 
+            this.button17.BackColor = System.Drawing.Color.Gold;
+            this.button17.Location = new System.Drawing.Point(578, 255);
+            this.button17.Name = "button17";
+            this.button17.Size = new System.Drawing.Size(99, 75);
+            this.button17.TabIndex = 17;
+            this.button17.Text = "Select your seat please";
+            this.button17.UseVisualStyleBackColor = false;
+            this.button17.Click += new System.EventHandler(this.Button17_Click);
+            // 
+            // button18
+            // 
+            this.button18.BackColor = System.Drawing.Color.Gold;
+            this.button18.Location = new System.Drawing.Point(578, 347);
+            this.button18.Name = "button18";
+            this.button18.Size = new System.Drawing.Size(99, 75);
+            this.button18.TabIndex = 18;
+            this.button18.Text = "Select your seat please";
+            this.button18.UseVisualStyleBackColor = false;
+            this.button18.Click += new System.EventHandler(this.Button18_Click);
+            // 
+            // button19
+            // 
+            this.button19.BackColor = System.Drawing.Color.Gold;
+            this.button19.Location = new System.Drawing.Point(752, 12);
+            this.button19.Name = "button19";
+            this.button19.Size = new System.Drawing.Size(99, 75);
+            this.button19.TabIndex = 19;
+            this.button19.Text = "Select your seat please";
+            this.button19.UseVisualStyleBackColor = false;
+            this.button19.Click += new System.EventHandler(this.Button19_Click);
+            // 
+            // count
+            // 
+            this.count.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.count.ForeColor = System.Drawing.Color.Aqua;
+            this.count.Location = new System.Drawing.Point(18, 72);
+            this.count.Margin = new System.Windows.Forms.Padding(30);
+            this.count.Name = "count";
+            this.count.Size = new System.Drawing.Size(92, 78);
+            this.count.TabIndex = 21;
+            this.count.Text = "0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(203, 32);
+            this.label1.TabIndex = 22;
+            this.label1.Text = "Seats choosed";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 191);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(148, 32);
+            this.label2.TabIndex = 24;
+            this.label2.Text = "Total price";
+            // 
+            // Price
+            // 
+            this.Price.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Price.ForeColor = System.Drawing.Color.Aqua;
+            this.Price.Location = new System.Drawing.Point(18, 237);
+            this.Price.Margin = new System.Windows.Forms.Padding(30);
+            this.Price.Name = "Price";
+            this.Price.Size = new System.Drawing.Size(182, 78);
+            this.Price.TabIndex = 23;
+            this.Price.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.ClientSize = new System.Drawing.Size(1299, 643);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Price);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.count);
+            this.Controls.Add(this.button19);
+            this.Controls.Add(this.button18);
+            this.Controls.Add(this.button17);
+            this.Controls.Add(this.button16);
+            this.Controls.Add(this.button15);
+            this.Controls.Add(this.button14);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.Button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Button1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button button14;
+        private System.Windows.Forms.Button button15;
+        private System.Windows.Forms.Button button16;
+        private System.Windows.Forms.Button button17;
+        private System.Windows.Forms.Button button18;
+        private System.Windows.Forms.Button button19;
+        private System.Windows.Forms.RichTextBox count;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox Price;
+    }
+}
+
---
+++ sinema/Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sinema
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public static int Counter = 0;
+        public  void SeatSelector(Button seats)
+        {
+            if (seats.BackColor == Color.Gold)
+            {
+                seats.BackColor = Color.DeepPink;
+                seats.Text = "Reserved";
+                Counter++;
+            }
+            else
+            {
+                seats.BackColor = Color.Gold;
+                seats.Text = "Select your seat please";
+                Counter--;
+            }
+            count.Text = Counter.ToString();
+            Price.Text = Counter * 5 + "AZN";
+        }
+        private void Button10_Click(object sender, EventArgs e)
+        {
+            SeatSelector(button10);
+        }
+
+        private void Button17_Click(object sender, EventArgs e)
+        {
+            SeatSelector(button17);
+        }
+
+        private void Button16_Click(object sender, EventArgs e)
+        {
+            SeatSelector(button16);
+        }
+
+        private void Button15_Click(object sender, EventArgs e)
+        {
+            SeatSelector(button15);
+        }
+
+        private void Button14_Click(object sender, EventArgs e)
+        {
+            SeatSelector(button14);
+        }
+
+        private void Button13_Click(object sender, EventArgs e)
+        {
+            SeatSelector(button13);
+        }
+
+        private void Button12_Click(object sender, EventArgs e)
+        {
+            SeatSelector(button12);
+        }
+
+        private void Button11_Click(object sender, EventArgs e)
+        {
+            SeatSelector(button11);
+        }
+
+        private void Button18_Click(object sender, EventArgs e)
+        {
+            SeatSelector(button18);
+        }
+
+        private void Button9_Click(object sender, EventArgs e)
+        {
+            SeatSelector(button9);
+        }
+
+        private void Button8_Click(object sender, EventArgs e)
+        {
+            SeatSelector(button8);
+        }
+
+        private void Button7_Click(object sender, EventArgs e)
+        {
+            SeatSelector(button7);
+        }
+
+        private void Button6_Click(object sender, EventArgs e)
+        {
+            SeatSelector(button6);
+        }
+
+        private void Button5_Click(object sender, EventArgs e)
+        {
+            SeatSelector(button5);
+        }
+
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            SeatSelector(button4);
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            SeatSelector(button3);
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            SeatSelector(button2);
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            SeatSelector(button1);
+        }
+        private void Button19_Click(object sender, EventArgs e)
+        {
+            SeatSelector(button19);
+        }
+
+        private void Button1_Click_1(object sender, EventArgs e)
+        {
+            SeatSelector(Button1);
+        }
+    }
+}
